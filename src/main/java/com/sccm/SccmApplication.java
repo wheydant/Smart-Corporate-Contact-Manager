12313bc2f6a8925edf1d65ca8daf02a533fe01ba
Vedant Karlekar <vedantkarlekar1@gmail.com>
2024-11-12 15:47:31 +0530

    Project Setup

---
+++ src/main/java/com/sccm/SccmApplication.java
@@ -0,0 +1,13 @@
+package com.sccm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SccmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SccmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sccm/SccmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sccm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SccmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
