2cff2cb1d7084a65215671e188a15a524f20ac85
Vedant Karlekar <vedantkarlekar1@gmail.com>
2024-11-13 11:21:42 +0530

    Basic setup

--- src/main/java/com/sccm/controllers/PageController.java
+++ src/main/java/com/sccm/controllers/PageController.java
@@ -1,6 +1,7 @@
 package com.sccm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         System.out.println("Home page Handler");
+        model.addAttribute("name", "Welcome to Smart Corporate Contact Manager");
+        model.addAttribute("paragraph", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum voluptatum quae amet optio adipisci voluptate, sint dolores minima consequuntur delectus, nihil sapiente consequatur praesentium saepe animi ducimus, esse architecto repellat a consectetur ipsum? Blanditiis repellendus eaque sint alias obcaecati molestiae magni quam quos voluptatibus assumenda deleniti voluptates, possimus et culpa?");
+        model.addAttribute("link", "https://youtu.be/SAqi7zmW1fY?si=8CpQ6drnVwMlMLYl");
         //This is basically name of view.
         return "home";
     }
